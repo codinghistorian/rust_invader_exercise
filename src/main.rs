dab4d4455d2af0615ded0474645373f875016cbf
codinghistorian <gaudihaus@gmail.com>
2023-09-20 15:21:38 +0400

    invaders_loop_with_shots

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
 use std::error::Error;
+use std::io;  // Corrected import for io module
+use std::sync::mpsc;
+use std::thread;
+
 use rusty_audio::Audio;
+use crossterm::terminal::{self, EnterAlternateScreen, LeaveAlternateScreen}; // Corrected import for terminal
+use crossterm::cursor::{Hide, Show};
+use crossterm::ExecutableCommand;
+use crossterm::event::{self, KeyCode, Event};
+use std::time::Duration;
+use std::time::Instant;
+use invaders::frame::new_frame;
+use invaders::render;
+use invaders::player::Player;
+use invaders::frame::Drawable;
+
 
 fn main() -> Result <(), Box<dyn Error>> {
     let mut audio = Audio::new();
@@ -11,7 +26,72 @@ fn main() -> Result <(), Box<dyn Error>> {
     audio.add("win", "win.wav");
     audio.play("startup");
 
+    // Terminal
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    // Render loop in a seperate thread
+    let (render_tx, render_rx) = mpsc::channel();
+    let render_handle = thread::spawn(move || {
+        let mut last_frame = invaders::frame::new_frame();
+        let mut stdout = io::stdout();
+        render::render(&mut stdout, &last_frame, &last_frame, true);
+        loop {
+            let curr_frame = match render_rx.recv() {
+                Ok(x) => x,
+                Err(_) => break,
+            };
+            render::render(&mut stdout, &last_frame, &curr_frame, false);
+            last_frame = curr_frame;
+        }
+    });
+
+    // Game Loop
+    let mut player = Player::new();
+    let mut instant = Instant::now();
+    'gameloop: loop {
+        // Per-frame init
+        let delta = instant.elapsed();
+        instant = Instant::now();
+        let mut curr_frame = new_frame();
+
+        // Input
+        while event::poll(Duration::default())? {
+            if let Event::Key(key_event) = event::read()? {
+                match key_event.code {
+                    KeyCode::Left => player.move_left(),
+                    KeyCode::Right => player.move_right(),
+                    KeyCode::Char(' ') | KeyCode::Enter => {
+                        if player.shoot() {
+                            audio.play("pew")
+                        }
+                    }
+                    KeyCode::Esc | KeyCode::Char('q') => {
+                        audio.play("lose");
+                        break 'gameloop;
+                    }
+                    _ => {}
+                }
+            }
+        }
+
+        // Updates
+        player.update(delta);
+
+        // Draw & render
+        player.draw(&mut curr_frame);
+        let _ = render_tx.send(curr_frame);
+        thread::sleep(Duration::from_millis(1));
+    }
+
     // CleanUp
+    drop(render_tx);
+    render_handle.join().unwrap();
     audio.wait();
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
     Ok(())
 }
---
+++ target/debug/deps/invaders-60a0b669aed22704.d
@@ -0,0 +1,5 @@
+/Users/sangjun.lee/Sangjun/rust_invader_exercise/target/debug/deps/invaders-60a0b669aed22704: src/main.rs
+
+/Users/sangjun.lee/Sangjun/rust_invader_exercise/target/debug/deps/invaders-60a0b669aed22704.d: src/main.rs
+
+src/main.rs:
